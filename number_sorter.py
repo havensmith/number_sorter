2a91e183c5b57040be4d5c1e0d2bfcfb535ec13c
havensmith <55714108+havensmith@users.noreply.github.com>
2020-01-07 16:08:29 -0600

    first commit

---
+++ number_sorter.py
@@ -0,0 +1,17 @@
+#Python Number Sorter
+#Haven Smith
+#7 January 2020
+#________________________________________________________________________
+numbers = []
+
+with open("unsorted_numbers.txt") as f:
+    for line in f:
+        numbers.append(int(line))
+
+numbers.sort()
+
+with open("sorted_numbers.txt", "w") as f:
+    for x in numbers:
+        f.write(str(x) + "\n")
+
+print("Numbers all sorted!")
